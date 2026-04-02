780eb7e3b37ae042952fffc158062afc183873de
ayuan <17628625842@aliyun.com>
2026-04-02 15:14:20 +0800

    feat

--- src/views/canvas.js
+++ src/views/canvas.js
@@ -19,7 +19,8 @@ function createUserTipCanvas(agentName, conversationId) {
                 { type: "button", label: "💵 $5", style: "primary", id: "tip_5", action: { type: "submit" } },
                 { type: "button", label: "💵 $10", style: "primary", id: "tip_10", action: { type: "submit" } },
                 { type: "button", label: "💵 $20", style: "primary", id: "tip_20", action: { type: "submit" } },
-                { type: "button", label: "✨ Custom Amount 11", style: "secondary", id: "tip_custom", action: { type: "url", url: 'https://mulebuy.com/my-account/user-center?id=123' } }
+                { type: "button", label: "✨ Custom Amount 11", style: "secondary", id: "tip_custom", action: { type: "url", url: 'https://mulebuy.com/my-account/user-center?id=123' } },
+                { type: "button", label: "✨ Custom Amount app", style: "secondary", id: "tip_custom", action: { type: "url", url: 'mulebuy://?id=8qdq&num=10' } },
             ]
         },
         metadata: { agentName, conversationId }
